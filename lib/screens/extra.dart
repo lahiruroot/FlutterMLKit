162fbf8543bbc1d94acea27d657c5a85a2ac148d
Lahiru Udawaththa <45584797+lahiruroot@users.noreply.github.com>
2021-08-17 16:49:59 +0530

    commit

---
+++ lib/screens/extra.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class Extra extends StatefulWidget {
+  const Extra({ Key? key }) : super(key: key);
+
+  @override
+  _ExtraState createState() => _ExtraState();
+}
+
+class _ExtraState extends State<Extra> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Texrtapp',),
+      ),
+      
+    );
+  }
+},
